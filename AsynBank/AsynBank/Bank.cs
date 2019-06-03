1687042b1ac05b938e5093f722e8769019777ad9
BarakpaevOlzhas <40425093+BarakpaevOlzhas@users.noreply.github.com>
2019-06-03 18:21:25 +0600

    Add files via upload

---
+++ AsynBank/AsynBank/Bank.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsynBank
+{
+    public class Bank
+    {
+        private int moneу;
+
+        private object lockObgect = new object();
+
+        private int minute = 0;
+
+        private Timer timer;
+
+        private DateTime stopTime;
+
+        public Bank(int moneyTwo)
+        {
+            moneу = moneyTwo;
+            TimerCallback timerCallback = new TimerCallback(ChangeSum);
+            timer = new Timer(timerCallback, 0, 0, 5);
+            stopTime = DateTime.Now.AddSeconds(60);
+            
+        }
+
+        public void ChangeSum(object objectL)
+        {
+            if (stopTime > DateTime.Now)
+            {
+                //lock (lockObgect)
+                //{
+                    var currentThread = Thread.CurrentThread;                   
+
+                    var random = new Random();
+                    int number = random.Next(0,100);
+
+                Console.WriteLine($"{currentThread.ManagedThreadId} было {moneу}");
+
+                if (number <= 50)
+                    {
+                        moneу += 500;
+                    }
+                    else
+                    {
+                        moneу -= 500;
+                    }
+
+                Console.WriteLine($"{currentThread.ManagedThreadId} изменил {moneу}");
+
+                Thread.Sleep(1000);
+
+                    minute += 5;
+
+                    Console.WriteLine($"\n");
+                //}
+            }
+            else
+            {
+                timer.Dispose();
+            }          
+
+        }
+    }
+}
---
+++ AsynBank/AsynBank/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsynBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopTime = DateTime.Now.AddMinutes(1);
+            var threads = new Thread[20];
+            var bank = new Bank(2000);
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                var thread = new Thread(bank.ChangeSum);
+                thread.Name = $"Поток номер { thread.ManagedThreadId}";
+                threads[i] = thread;
+            }
+
+            foreach (var i in threads)
+            {
+                i.Start();
+            }
+
+            Console.Read();
+        }
+    }
+}
